1c0ddfd27d66a3d8ff0a1e3590675bc065e4bac7
Antonio Martinez Sanabria <tonytower@iMac-de-Antonio-Martinez-Sanabria.local>
2015-02-10 18:00:47 +0100

    Initial Commit

---
+++ E2T1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  E2T1
+//
+//  Created by Antonio Martinez Sanabria on 10/02/15.
+//  Copyright (c) 2015 Antonio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ E2T1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  E2T1
+//
+//  Created by Antonio Martinez Sanabria on 10/02/15.
+//  Copyright (c) 2015 Antonio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
